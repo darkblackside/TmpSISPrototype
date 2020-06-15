d8c658af63fbca946bf9f02eb13e37100147b06c
darkblackside <merschi.bjoern@web.de>
2020-06-15 16:55:22 +0200

    Finalized prototype

--- basicRuleEngine.js
+++ basicRuleEngine.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const app = express();
 const bodyParser = require("body-parser");
+const request = require('request');
 
 const valuesMap = new Map();
-const suggestionCatalogueUrl = "http://localhost";
+const suggestionCatalogueUrl = "http://localhost:4000/";
 
 app.get('/', function (req, res) {
     res.json(Array.from(valuesMap.keys()));
@@ -24,9 +25,15 @@ app.use(bodyParser.text());
 app.post('/:itemname', function (req, res) {
     var itemname = req.params.itemname;
     if(req.body) {
+        oldValue = valuesMap.get(itemname);
         valuesMap.set(itemname, req.body);
+
+        if(!oldValue || oldValue != req.body) {
+            console.log(req.body);
+            sendSuggestions();
+        }
+
         res.send(req.body);
-        sendSuggestions();
     } else {
         res.status(400).send("Unsupported format, please provide item in URL and value in body");
     }
@@ -35,12 +42,30 @@ app.post('/:itemname', function (req, res) {
 function sendSuggestions() {
     if(valuesMap.has('insideTemp') &&
         valuesMap.has('outsideTemp') &&
-        valuesMap.has('diffTemp')) {
-        if(valuesMap.get('diffTemp') > 0 && valuesMap.get('insideTemp') > 25) {
-            // TODO send suggestion to open windows to suggestionCatalogueUrl
-            console.log("Suggestion: Open window")
+        valuesMap.has('diffTemp') &&
+        valuesMap.has('windowOpen')) {
+        if(valuesMap.get('diffTemp') > 0 && valuesMap.get('insideTemp') > 25 && valuesMap.get('windowOpen') == 'False') {
+            sendSuggestion("openWindows", "Windows", "Open Windows", "Temperature too high");
         }
     }
+    // More rules go in here!
+}
+
+function sendSuggestion(identifier, name, suggestion, reason) {
+    var options = {
+        'method': 'POST',
+        'url': suggestionCatalogueUrl + identifier,
+        'headers': {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({"name":name,"suggestion":suggestion,"reason":reason})
+        
+    };
+    console.log(options.url);
+    request(options, function (error, response) { 
+    if (error) throw new Error(error);
+        console.log(response.body);
+    });
 }
 
 app.listen(3000, function () {
--- getTemps.py
+++ getTemps.py
@@ -1,6 +1,13 @@
-import requests 
+#!/usr/local/bin/python
+# -*- coding: latin-1 -*-
 
-resultUrl = "http://127.0.0.1:3000/"
+# pip install requests
+# pip install gpiozero
+import requests 
+from gpiozero import Button
+from time import sleep
+
+resultUrl = "http://192.168.192.34:3000/"
 
 URL = "http://192.168.192.20:8080/rest/items/Temp_InsideTemperature"
 r = requests.get(url = URL) 
@@ -10,13 +17,16 @@ URL = "http://192.168.192.20:8080/rest/items/Temp_OutsideTemperature"
 r = requests.get(url = URL) 
 data2 = r.json()['state']
 
-# todo: get inside temperature as a middle value of netatmo sensor & raspi sensor
-insideTemp = float(data1.replace('°C', '').strip())
+sensor = Button(14)
 
+while True:
+    pressed = sensor.is_pressed
+    insideTemp = float(data1[:-2].strip())
+    outsideTemp = float(data2[:-2].strip())
+    diffTemp = insideTemp - outsideTemp
 
-outsideTemp = float(data2.replace('°C', '').strip())
-diffTemp = insideTemp - outsideTemp
-
-x = requests.post(resultUrl + "insideTemp", data = str(insideTemp), headers = {"Content-Type":"text/plain"})
-x = requests.post(resultUrl + "outsideTemp", data = str(outsideTemp), headers = {"Content-Type":"text/plain"})
-x = requests.post(resultUrl + "diffTemp", data = str(diffTemp), headers = {"Content-Type":"text/plain"})
+    x = requests.post(resultUrl + "insideTemp", data = str(insideTemp), headers = {"Content-Type":"text/plain"})
+    x = requests.post(resultUrl + "outsideTemp", data = str(outsideTemp), headers = {"Content-Type":"text/plain"})
+    x = requests.post(resultUrl + "diffTemp", data = str(diffTemp), headers = {"Content-Type":"text/plain"})
+    x = requests.post(resultUrl + "windowOpen", data = str(pressed), headers = {"Content-Type":"text/plain"})
+    sleep(2000)
